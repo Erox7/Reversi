0971f76c32615b61b26ab707517f1adff7e75614
Sergio Salcedo <Sergio Salcedo>
2017-04-19 00:13:05 +0200

    FirstCommit

---
+++ app/src/main/java/com/example/erox/reversi/DesarrolloActivity.java
@@ -0,0 +1,13 @@
+package com.example.erox.reversi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DesarrolloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desarrollo);
+    }
+}
